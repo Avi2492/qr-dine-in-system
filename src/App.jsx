00e33f7937af2df1888793cdc063c38bc209e192
Avinash Pandey <avinash.12009084@lpu.in>
2024-01-19 23:37:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <h1 className="text-xl flex justify-center items-center h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+      Hello, I am QR based Order Place System 👨‍💻 Lets Build Together and I am
+      Here to reduce the Queues on Restaurents
+    </h1>
+  );
+};
+
+export default App;
