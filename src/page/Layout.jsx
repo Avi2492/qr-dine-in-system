ffa71e88c44ac078f448c37f54ebf40224b90910
Avinash Pandey <avinash.12009084@lpu.in>
2024-03-03 21:07:38 +0530

    Logo and Some changes

--- src/page/Layout.jsx
+++ src/page/Layout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Layout = () => {
   return (
-    <div className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% ... h-screen text-center flex justify-center text-xl bg-slate-500">
+    <div className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% h-screen text-center flex justify-center items-center text-5xl bg-slate-500">
       Hey there We are building new software which can take order online to
       reduce the queues. 🍰
     </div>
